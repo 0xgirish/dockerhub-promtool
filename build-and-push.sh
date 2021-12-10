dc5af7be41918519cb439f884376d0a982bf7750
Girish Kumar <zkmrgirish@gmail.com>
2021-12-10 16:16:04 +0530

    fix
    Signed-off-by: Girish Kumar <zkmrgirish@gmail.com>

--- build-and-push.sh
+++ build-and-push.sh
@@ -5,5 +5,5 @@ if test $RELEASE_VERSION = "main"; then
 fi
 
 # build and push
-docker build --build-arg checkout_point=$checkout_point -t zkmrgirish/promtool:${RELEASE_TAG}
-docker push zkmrgirish/promtool:${RELEASE_TAG}
+docker build --build-arg checkout_point=$checkout_point -t zkmrgirish/promtool:${RELEASE_VERSION}
+docker push zkmrgirish/promtool:${RELEASE_VERSION}
